134a332560e99c29ca24746ccbc18ef50fed449a
myst-6 <michaelbjhall@gmail.com>
2024-08-23 23:25:33 +0100

    add light/dark mode and switch

--- components/Link/index.tsx
+++ components/Link/index.tsx
@@ -1,16 +1,20 @@
 import NextLink from "next/link";
 import { Link as ChakraLink, type LinkProps as ChakraLinkProps } from "@chakra-ui/next-js";
+import { useColorModePreference } from "@chakra-ui/react";
 
 export interface LinkProps extends ChakraLinkProps {
   current?: boolean;
 }
 
 export const Link = ({ children, current, ...props }: LinkProps) => {
+  const colorMode = useColorModePreference();
+
   return <ChakraLink 
     as={NextLink} 
     borderRadius={5}
     padding={1}
-    _hover={current ? {} : { backgroundColor: "gray.100" }} 
+    color={colorMode === "dark" ? "lightblue" : "blue"}
+    _hover={current ? {} : { backgroundColor: colorMode === "dark" ? "gray.600" : "gray.100" }} 
     {...props}>
       {children}
   </ChakraLink>;
