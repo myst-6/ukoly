958ae696d65eb97dded204566cdca2f6d18b09b7
Alex Pylypenko <macaquedev@gmail.com>
2024-08-24 22:26:57 +0100

    Fixed

--- solutions/bio1/DreamingSpires/index.tsx
+++ solutions/bio1/DreamingSpires/index.tsx
@@ -19,15 +19,15 @@ export const DreamingSpires = () => {
       </SText>
       <SText>
         To keep track of how many moves it took to reach the current state, a <SCode>dist</SCode> variable can be added alongside the state when it is pushed to the queue. That variable is incremented by $1$ when each move is carried out to get the neighbouring states.
-        In the sample code, this was achieved by making the BFS queue a queue of <SCode>std::pair</SCode> structures, which store the state and <SCode>dist</SCode>. Once the finish state is encountered, the minimum number of moves is the accompanying <SCode>dist</SCode> variable.
+        In the sample code, this was achieved by making the BFS queue a queue of pairs, which store the state and <SCode>dist</SCode>. Once the finish state is encountered, the minimum number of moves is the accompanying <SCode>dist</SCode> variable.
       </SText>
       <SText>
         A key observation is that if you can go from one state to another state by moving a ball from peg $A$ to peg $B$, then you can go back to the original state by moving a ball from peg $B$ to peg $A$. This poses an issue
         for our BFS algorithm, as it could cause an infinite loop where a neighbour state is added to the BFS queue when a state is traversed, and the original is added when neighbouring state is traversed.
       </SText>
       <SText>
-        This can be prevented by keeping a store of which states have been traversed already, and checking if the state popped from the queue is present. If so, the current state is dropped and the next element is popped from the queue.
-        In the sample code, this was achieved using an <SCode>std::set</SCode> called <SCode>visited</SCode> and by checking if the state popped is present in <SCode>visited</SCode> at the beginning of each iteration.
+        This can be prevented by keeping a store of which states have been visited already, and checking if the state popped from the queue is present. If so, the current state is dropped and the next element is popped from the queue.
+        In the sample code, this was achieved using a set called <SCode>visited</SCode> and by checking if the state popped is present in <SCode>visited</SCode> at the beginning of each iteration.
       </SText>
       <SText>
         This editorial was a brief explanation of BFS in the context of this problem. For a more rigorous coverage, visit<Link href="https://usaco.guide/silver/graph-traversal?lang=cpp">this USACO Guide page</Link>.
