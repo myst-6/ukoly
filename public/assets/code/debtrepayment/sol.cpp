e1a80f840a0b356adadce154a1befbf6eacb5f4b
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-08-24 19:52:07 +0100

    added bio1 2018 q1

---
+++ public/assets/code/debtrepayment/sol.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    float interest, repayment; cin >> interest >> repayment;
+    interest = interest / 100 + 1;
+    repayment /= 100;
+
+    int debt = 10000, ans = 0;
+    while (debt > 0) {
+        debt = ceil(debt*interest);
+        int repaid = min(max((int) ceil(debt * repayment), 5000), debt);
+        debt -= repaid;
+        ans += repaid;
+    }
+    printf("%.2f", ans/100.0);
+
+    return 0;
+}
