a53b60cfb92141d6b6213b8bcd3ac1d563df536e
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-08-25 18:03:19 +0100

    actually fixed typography (subtitle)

--- components/SSubtitle/index.tsx
+++ components/SSubtitle/index.tsx
@@ -2,7 +2,7 @@ import { SText, type STextProps } from "../SText";
 
 export type SSubtitleProps = STextProps;
 
-export const SSubtitle = ({ typography = "body.medium", children, ...props }: STextProps) => {
+export const SSubtitle = ({ typography = "title.medium", children, ...props }: STextProps) => {
   return <SText typography={typography} mt={4} {...props}>
     {children}
   </SText>
