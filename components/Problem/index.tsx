bd54cff0da18778d19f312202a9a18e995036cd4
myst-6 <michaelbjhall@gmail.com>
2024-08-25 11:31:14 +0100

    change design of problem cards; add divider and fix height

--- components/Problem/index.tsx
+++ components/Problem/index.tsx
@@ -1,9 +1,10 @@
+import { Box } from "../Box";
 import { Badge } from "../Badge";
-import { Button } from "../Button";
 import { Card, type CardProps } from "../Card";
+import { Button } from "../Button";
+import { Divider } from "../Divider";
 import { Flex } from "../Flex";
-import { HStack } from "../Stack";
-import { Spacer } from "../Spacer";
+import { HStack, VStack } from "../Stack";
 import { Text } from "../Text";
 import { Wrap } from "../Wrap";
 import { difficultyColors, ProblemInfo } from "content";
@@ -15,33 +16,39 @@ export interface ProblemProps extends CardProps {
 
 export const Problem = ({ problem, onChoose, ...props }: ProblemProps) => {
   return (
-    <Card width="sm" {...props}>
-      <HStack mb={2}>
-        <Text typography="title.large">{problem.display}</Text>
-        <Flex flex={1} justifyContent="flex-end">
-          <Button 
-            size="sm" 
-            colorScheme={difficultyColors[problem.difficulty]} 
-            onClick={() => onChoose()}
-          >
-            View
-          </Button>
-        </Flex>
-      </HStack>
-      Editorial authored by {problem.editorialAuthors.join(", ")}.
-      Code provided by {problem.solutionAuthors.join(", ")}.
-      <Spacer margin={2} />
-      <Wrap>
-        <Badge colorScheme={difficultyColors[problem.difficulty]}>{problem.difficulty}</Badge>
-        <Badge>{problem.year}</Badge>
-        {
-          ...problem.tags.map(tag => {
-            return (
-              <Badge key={tag}>{tag}</Badge>
-            );
-          })
-        }
-      </Wrap>
+    <Card width="sm" height="3xs" {...props}>
+      <VStack height="100%" justifyContent="space-between">
+        <Box>
+          <HStack width="100%" mb={2}>
+            <Text typography="title.large">{problem.display}</Text>
+            <Flex flex={1} justifyContent="flex-end">
+              <Button 
+                size="sm" 
+                colorScheme={difficultyColors[problem.difficulty]} 
+                onClick={() => onChoose()}
+              >
+                View
+              </Button>
+            </Flex>
+          </HStack>
+          <Divider m={2} />
+          <Text typography="body.medium">
+            Editorial authored by {problem.editorialAuthors.join(", ")}.
+            Code provided by {problem.solutionAuthors.join(", ")}.
+          </Text>
+        </Box>
+        <Wrap>
+          <Badge colorScheme={difficultyColors[problem.difficulty]}>{problem.difficulty}</Badge>
+          <Badge>{problem.year}</Badge>
+          {
+            ...problem.tags.map(tag => {
+              return (
+                <Badge key={tag}>{tag}</Badge>
+              );
+            })
+          }
+        </Wrap>
+      </VStack>
     </Card>
   );
 };
