140fcf49f0254cf5f06581fadbb34f6bc6b3e9c4
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-08-25 18:03:50 +0100

    fixed title typography

--- components/STitle/index.tsx
+++ components/STitle/index.tsx
@@ -2,8 +2,8 @@ import { SText, type STextProps } from "../SText";
 
 export type STitleProps = STextProps;
 
-export const STitle = ({ typography = "body.medium", children, ...props }: STextProps) => {
-  return <SText typography="title.large" mt={4} {...props}>
+export const STitle = ({ typography = "title.large", children, ...props }: STextProps) => {
+  return <SText typography={typography} mt={4} {...props}>
     {children}
   </SText>
 };
