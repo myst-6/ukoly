faa5bc2956f037b64c1b516dd31ed51f859733cc
myst-6 <michaelbjhall@gmail.com>
2024-08-26 00:54:51 +0100

    formatting changes

--- solutions/bio2/KeepOnTheGrass/index.tsx
+++ solutions/bio2/KeepOnTheGrass/index.tsx
@@ -15,8 +15,8 @@ export const KeepOnTheGrass = () => {
       </SText>
       <STitle>Observation 2</STitle>
       <SText>
-        Can you easily map some subset of the grid in the
-        diagram for day $2^n$ onto the grid for day $2^{brace("n+1")}$?
+        You can map some subset of the grid in the
+        diagram for day $2^n$ onto the grid for day $2^{brace("n+1")}$.
       </SText>
 
       <STitle>Solution</STitle>
@@ -28,48 +28,48 @@ export const KeepOnTheGrass = () => {
         This is shown in the diagrams below, going from day $4$ to day $8$ on the left,
         and from day $8$ to day $16$ on the right.
       </SText>
-      <HStack alignItems="flex-start" paddingTop="20px">
-        <VStack m={2} justify="space-between">
-          <Image src="/assets/images/keeponthegrass/grass4.png" width="100rem" alt="Sample Diagram" />
-          <Image src="/assets/images/keeponthegrass/grass5.png" width="30rem" alt="Sample Diagram" />
+      <HStack mt={2} gap={4} align="stretch">
+        <VStack justify="space-around">
+          <Image src="/assets/images/keeponthegrass/grass4.png" width="60rem" alt="Sample Diagram" />
+          <Image src="/assets/images/keeponthegrass/grass5.png" width="60rem" alt="Sample Diagram" />
         </VStack>
-        <VStack m={2} justify="space-between">
-        <SText>
-          When the squares are colour-coded like this, it is
-          much easier to solve the problem, because it is
-          clear that each square on day ${brace("2^{n+1}")}$
-          is either:
-        </SText>
-        <SList>
+        <VStack>
           <SText>
-            Not grassed (white on the diagram), in which case
-            the square is unreachable.
+            When the squares are colour-coded like this, it is
+            much easier to solve the problem, because it is
+            clear that each square on day ${brace("2^{n+1}")}$
+            is either:
           </SText>
-          <SText>
-            Black, in which case the square can directly be
-            mapped to a square on day $2^n$, and the answer
-            is $2f({brace("\\frac{X}{2}")}, {brace("\\frac{Y}{2}")})$,
-            because the black squares are twice as far apart 
-            on day $2^{brace("n+1")}$ as they are on day $2^n$.
-          </SText>
-          <SText>
-            Red, in which case the square is guaranteed to be 
-            one square away from a black square, so the answer
-            is $1 + f(n_x, n_y)$, where $(n_x, n_y)$ are the
-            coordinates of the adjacent black square.
-          </SText>
-          <SText>
-            Green, in which case the square is guaranteed to be
-            one square away from two black squares, so the answer
-            is $1 + min(f(a_x, a_y), f(b_x, b_y))$, where
-            $(a_x, a_y)$ and $(b_x, b_y)$ are the coordinates
-            of the adjacent black squares.
-          </SText>
-        </SList>
+          <SList ml={5}>
+            <SText>
+              Not grassed (white on the diagram), in which case
+              the square is unreachable.
+            </SText>
+            <SText>
+              Black, in which case the square can directly be
+              mapped to a square on day $2^n$, and the answer
+              is $2f({brace("\\frac{X}{2}")}, {brace("\\frac{Y}{2}")})$,
+              because the black squares are twice as far apart 
+              on day $2^{brace("n+1")}$ as they are on day $2^n$.
+            </SText>
+            <SText>
+              Red, in which case the square is guaranteed to be 
+              one square away from a black square, so the answer
+              is $1 + f(n_x, n_y)$, where $(n_x, n_y)$ are the
+              coordinates of the adjacent black square.
+            </SText>
+            <SText>
+              Green, in which case the square is guaranteed to be
+              one square away from two black squares, so the answer
+              is $1 + min(f(a_x, a_y), f(b_x, b_y))$, where
+              $(a_x, a_y)$ and $(b_x, b_y)$ are the coordinates
+              of the adjacent black squares.
+            </SText>
+          </SList>
         </VStack>
-        <VStack m={2} justify="space-between">
-          <Image src="/assets/images/keeponthegrass/grass2.png" width="100rem" alt="Sample Diagram" />
-          <Image src="/assets/images/keeponthegrass/grass.png" width="30rem" alt="Sample Diagram" />
+        <VStack justify="space-around">
+          <Image src="/assets/images/keeponthegrass/grass2.png" width="60rem" alt="Sample Diagram" />
+          <Image src="/assets/images/keeponthegrass/grass.png" width="60rem" alt="Sample Diagram" />
         </VStack>
       </HStack>
       <STitle>Small implementation detail</STitle>
@@ -78,7 +78,7 @@ export const KeepOnTheGrass = () => {
         the red and green cases into one, by modifying the function
         to return {<SCode>LLONG_MAX</SCode>} when a square is unreachable,
         so for every red or green square, we can just return 
-        {<SCode>1 + min(f(a_x, a_y), f(b_x, b_y))</SCode>}.
+        $1 + \min(f(a_x, a_y), f(b_x, b_y))$.
       </SText>
       <STitle>Conclusion</STitle>
       <SText>
