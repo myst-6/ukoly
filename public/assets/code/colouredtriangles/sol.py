d2007c201ac1bfc23928598287f5303219e329af
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-08-26 10:50:00 +0100

    Removed set xor, replaced with map

--- public/assets/code/colouredtriangles/sol.py
+++ public/assets/code/colouredtriangles/sol.py
@@ -1,12 +1,18 @@
 inp = input()
 curr = list(inp)
 
+colours = {
+    ('B', 'G'): 'R',
+    ('B', 'R'): 'G',
+    ('G', 'R'): 'B'
+}
+
 while (len(curr) > 1):
     new = []
     for i in range(0, len(curr)-1):
         c1, c2 = curr[i], curr[i+1]
         if c1 == c2: new.append(c1)
-        else: new.append(list({c1, c2}^{"R", "G", "B"})[0])
+        else: new.append(colours[(min(c1, c2), max(c1, c2))])
     curr = new
 
 print(curr[0])
