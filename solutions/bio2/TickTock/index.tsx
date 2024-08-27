54105610a4208900163ffd8a3c32f3817296c4d9
vladimirfilip <vlad.filip.self@gmail.com>
2024-08-27 15:14:11 +0100

    Update index.tsx

--- solutions/bio2/TickTock/index.tsx
+++ solutions/bio2/TickTock/index.tsx
@@ -24,16 +24,16 @@ export const TickTock = () => {
         The total audible period can be calculated by summing $l - f + 1$ over all merged intervals, and the number of ticks in each interval can then be calculated by subtracting the number of ticks in ${brace("[")}0, f{brace(")")}$ from the number of ticks in ${brace("[")}0, l + 1{brace(")")}$.
       </SText>
       <SText>
-        Now for the formula to calculate ticks in the range ${brace("[")}0, n{brace(")")}$. Given a number $m$, the number of ticks of the form $10m + r$, $0 \leq r \lt 10$, is always $5$, since if the sum of the digits of $m$ is even, the values of $r$ that yield a tick are $0$, $2$, $4$, $6$, and $8$, and if the sum is odd, then the values of $r$ that yield a tick are $1$, $3$, $5$, $7$ and $9$.
+        Now for the formula to calculate ticks in the range ${brace("[")}0, n{brace(")")}$. Given a number $m$, the number of ticks of the form $10m + r$, $0 \leq r \lt 10$, is always $5$, since if the sum of the digits of $m$ is even, the values of $r$ that yield a tick are $0$, $2$, $4$, $6$, and $8$, and if the sum is odd, the values of $r$ that yield a tick are $1$, $3$, $5$, $7$ and $9$.
       </SText>
       <SText>
-        Let $n = 10m_n + r_n$. There are $m_n$ values of $m$ that yield a time value lower than $n$, as the size of the range ${brace("[")}0, m_n{brace(")")} = m_n$, and for each of these values of $m$ there are $5$ solutions of $r$ that yield a tick.
+        Let $n = 10m + r$. The range ${brace("[")}0, 10m{brace(")")}$ can be split into $m$ ranges ${brace("[")}0, 10{brace(")")}, {brace("[")}10, 20{brace(")")}, ... \, , {brace("[")}10{brace("(")}m - 1{brace(")")}, 10m{brace(")")}$, and in each of these ranges there are $5$ ticks.
       </SText>
       <SText>
-        Therefore, the number of ticks in ${brace("[")}0, m_n{brace(")")} = 5m_n$.
+        Therefore, the number of ticks in ${brace("[")}0, 10m{brace(")")} = 5m$.
       </SText>
       <SText>
-        The rest of the ticks in ${brace("[")}0, n{brace(")")}$ are in the range ${brace("[")}m_n, m_n + r_n{brace(")")}$. Since $0 \leq r_n \lt 10$, these can be counted brute-force by iterating over all values of $r \in {brace("[")}0, r_n {brace(")")}$ and adding it to the sum of the digits of $m_n$. This process is simplified in the sample code by only storing the parity of the sum of the digits of $m_n$ as we only need to check if the sum of the digits is even.
+        The rest of the ticks in ${brace("[")}0, n{brace(")")}$ are in the range ${brace("[")}10m, 10m + r{brace(")")}$. Since $0 \leq r \lt 10$, these can be counted brute-force by iterating over each value in the range ${brace("[")}0, r {brace(")")}$, adding it to the sum of the digits of $m$ and checking if that sum is even. This process is simplified in the sample code by only storing the parity of the sum of the digits of $m$.
       </SText>
       <SText>
         This gives us an iterative algorithm for finding the number of ticks in ${brace("[")}0, n{brace(")")}$ in $\mathcal{brace("O")}(\log n)$ time and space complexity. Given we have at most $2^{brace("15")} \approx 32\,000$ intervals to go through, the algorithm is more than fast enough to run within the time limit.
