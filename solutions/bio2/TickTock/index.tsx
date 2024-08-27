f95cbcd5b580552895d28497e2d71ebc9050ea51
vladimirfilip <vlad.filip.self@gmail.com>
2024-08-27 13:10:47 +0100

    yarn build no longer throws error

--- solutions/bio2/TickTock/index.tsx
+++ solutions/bio2/TickTock/index.tsx
@@ -1,4 +1,4 @@
-import { SCode, SCodeBlock, SText, STitle } from "components";
+import { SCodeBlock, SText, STitle } from "components";
 import { brace } from "solutions/util";
 
 export const TickTock = () => {
