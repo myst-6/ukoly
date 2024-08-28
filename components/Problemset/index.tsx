bb7113202bd3b7cc6766eb7b5edd2f52a44edaee
myst-6 <michaelbjhall@gmail.com>
2024-08-28 22:28:29 +0100

    collapse right

--- components/Problemset/index.tsx
+++ components/Problemset/index.tsx
@@ -21,16 +21,21 @@ export const Problemset = ({ problems }: ProblemsetProps) => {
   const [width, setWidth] = useState<number>(0);
   const [drag, setDrag] = useState<boolean>(false);
 
-  const [collapse, setCollapse] = useState<boolean>(false);
-  const [limit, setLimit] = useState<number>(0);
-  
+  const [collapseLeft, setCollapseLeft] = useState<boolean>(false);
+  const [limitLeft, setLimitLeft] = useState<number>(0);
+
+  const [collapseRight, setCollapseRight] = useState<boolean>(false);
+  const [limitRight, setLimitRight] = useState<number>(0);
+
   const grabRef = useRef<HTMLDivElement>(null);
-  const cardRef = useRef<HTMLDivElement>(null);
+  const cardRefLeft = useRef<HTMLDivElement>(null);
+  const cardRefRight = useRef<HTMLDivElement>(null);
 
   const onMouseMove = (ev: MouseEvent) => {
     if (!drag) return;
     if (grabRef.current === null) return;
-    if (cardRef.current === null) return;
+    if (cardRefLeft.current === null) return;
+    if (cardRefRight.current === null) return;
 
     // get x-coordinate of left side of box
     const { left } = ev.currentTarget.getBoundingClientRect();
@@ -42,16 +47,32 @@ export const Problemset = ({ problems }: ProblemsetProps) => {
     // go halfway into the bar
     const x = ev.clientX - left - pl;
 
-    // check if in collapsing territory
-    const { width } = getComputedStyle(cardRef.current);
-    const w = parseFloat(width);
-    if (!collapse && x <= w / 4) {
-      setCollapse(true);
-      setLimit(Math.max(x + 2, w / 4) + 2); // +2 to prevent flicker
-    } else {
-      if (collapse && x >= limit) setCollapse(false);
-      setWidth(x);
+    // check if in collapsing territory for left side
+    {
+      const { width } = getComputedStyle(cardRefLeft.current);
+      const w = parseFloat(width);
+      if (!collapseLeft && x <= w / 4) {
+        setCollapseLeft(true);
+        setLimitLeft(Math.max(x + 2, w / 4)); // +2 to prevent flicker
+      } else if (collapseLeft && x >= limitLeft) {
+        setCollapseLeft(false);
+      }
     }
+
+    // check if in collapsing territory for right side
+    {
+      const { left: side } = cardRefRight.current.getBoundingClientRect();
+      const { width } = getComputedStyle(cardRefRight.current);
+      const w = parseFloat(width);
+      if (!collapseRight && x >= side - left + 3 * w / 4) {
+        setCollapseRight(true);
+        setLimitRight(Math.min(x - 2, side - left + 3 * w / 4)); // -2 to prevent flicker
+      } else if (collapseRight && x <= limitRight) {
+        setCollapseRight(false);
+      }
+    }
+
+    setWidth(x);
   };
 
   return (
@@ -63,13 +84,14 @@ export const Problemset = ({ problems }: ProblemsetProps) => {
       cursor={drag ? "col-resize" : "auto"}
     >
       <Card 
-        variant={collapse ? "filled" : "outline"}
+        variant={collapseLeft ? "filled" : "outline"}
         m={0}
-        cardRef={cardRef}
-        width={`${collapse ? 8 : width}px`} 
-        minW={collapse ? "8px" : "fit-content"}
-        cursor={collapse ? "col-resize" : "inherit"}
-        onMouseDown={() => setDrag(collapse)}
+        cardRef={cardRefLeft}
+        flex={collapseRight ? 1 : undefined}
+        width={`${collapseLeft ? 8 : width}px`} 
+        minW={collapseLeft ? "8px" : "fit-content"}
+        cursor={collapseLeft ? "col-resize" : "inherit"}
+        onMouseDown={() => setDrag(collapseLeft)}
       >
         <VStack 
           flex="0" 
@@ -97,15 +119,19 @@ export const Problemset = ({ problems }: ProblemsetProps) => {
         p={1}
         flex={0} 
         ref={grabRef}
-        cursor={collapse ? "col-resize" : "col-resize"}
+        cursor={(collapseLeft || collapseRight) ? "col-resize" : "col-resize"}
         onMouseDown={() => setDrag(true)}
       >
         <Divider orientation="vertical" />
       </Spacer>
       <Card 
-        variant="outline" 
-        flex="1"
-        minW="md"
+        variant={collapseRight ? "filled" : "outline"}
+        cardRef={cardRefRight}
+        flex={collapseRight ? 0 : 1}
+        width={collapseRight ? "8px" : "auto"} 
+        minW={collapseRight ? "8px" : "md"}
+        cursor={collapseRight ? "col-resize" : "inherit"}
+        onMouseDown={() => setDrag(collapseRight)}
       >
         <VStack maxWidth="full" spacing={1} maxH="65vh" alignItems="left">
           <Text p={1}align="left" typography="display.small">Problem Viewer{problem && `: ${problem.display}`}</Text>
