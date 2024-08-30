42d6fa9eef2938627cfd2061271be12a204088cd
myst-6 <michaelbjhall@gmail.com>
2024-08-30 22:38:32 +0100

    responsive text

--- components/Text/index.tsx
+++ components/Text/index.tsx
@@ -3,15 +3,10 @@ import {
   Text as ChakraText,
   TextProps as ChakraTextProps,
 } from "@chakra-ui/react";
-import {
-  fonts,
-  fontSizes,
-  fontWeights,
-  Typography,
-} from "theme";
+import { fonts, fontSizes, fontWeights, Typography } from "theme";
 
 export interface TextProps extends ChakraTextProps {
-  typography: Typography;
+  typography: Typography | (Typography | null)[];
   children: React.ReactNode;
 }
 
@@ -19,9 +14,21 @@ export const Text = forwardRef<TextProps, "p">(
   ({ typography, children, ...props }, ref) => {
     return (
       <ChakraText
-        fontFamily={fonts[typography]}
-        fontWeight={fontWeights[typography]}
-        fontSize={fontSizes[typography]}
+        fontFamily={
+          Array.isArray(typography)
+            ? typography.map(typ => typ === null ? null : fonts[typ])
+            : fonts[typography]
+        }
+        fontWeight={
+          Array.isArray(typography)
+            ? typography.map(typ => typ === null ? null : fontWeights[typ])
+            : fontWeights[typography]
+        }
+        fontSize={
+          Array.isArray(typography)
+            ? typography.map(typ => typ === null ? null : fontSizes[typ])
+            : fontSizes[typography]
+        }
         ref={ref}
         {...props}
       >
