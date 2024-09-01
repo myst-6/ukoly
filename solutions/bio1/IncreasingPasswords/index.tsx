4b5e6735f3d445835b4a6418a09c84c427a75675
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-09-01 11:34:48 +0100

    boris changes

--- solutions/bio1/IncreasingPasswords/index.tsx
+++ solutions/bio1/IncreasingPasswords/index.tsx
@@ -12,8 +12,8 @@ export const IncreasingPasswords = () => {
       <STitle>Solution</STitle>
       <SText>
         Let's first define a recursive function that gets the number of passwords of length $n$, with all of its letters being greater than $c$.
-        To do this, we can first define the base case for when the length is 1. 
-        The number of ways to generate a password of length 1 with all characters greater than $c$: $36 - c$.
+        To do this, we can first define the base case for when the length is $1$. 
+        The number of ways to generate a password of length $1$ with all characters greater than $c$: $36 - c$.
         Otherwise, we can recursively call the function with the length $n-1$ and the character $c+i$ for all values of $i$ such that $c+i \leq 36$ ($1$-indexed).
         We can notice that the number of ways to generate a password of length $n$ with the first letter being $c$ is $f(n, c) - f(n, c-1)$.
       </SText>
@@ -25,9 +25,8 @@ export const IncreasingPasswords = () => {
       <SText>
         Now we can recursively build the $n$-th password. 
         We can first set up a base case, where the length of the password is $1$. The output here should be the $n - ways$-th letter possible ($1$-indexed). 
-        To recursively build the password, we can iterate through each possible character, and check if the number of ways to build it and all the passwords before it is greater than $n$ 
-        (it may be useful to think of this like binary/tenary search, with more intervals). 
-        If it is greater, we know that the current letter is the next letter in the result, and so we can append it, and recursively solve for $length - 1$.
+        To recursively build the password, we can iterate through each possible character, and check if the number of ways to build it and all the passwords before it is greater than or equal to $n$ 
+        If it is at least $n$, we know that the current letter is the next letter in the result, and so we can append it, and recursively solve for $length - 1$.
         We can finally output the result.
       </SText>
 
