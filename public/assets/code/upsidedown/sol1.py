c5ad6b0215b666225b8413af4300060d4ed2bf62
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-09-01 18:52:23 +0100

    added bio1 2011 q3

---
+++ public/assets/code/upsidedown/sol1.py
@@ -0,0 +1,25 @@
+from functools import lru_cache
+
+@lru_cache(None)
+def combs(length):
+    if length <= 1: return 1
+    return 9 * combs(length - 2)
+
+def solve(length):
+    global ways
+    if length == 0: return ''
+    if length == 1: return '5'
+
+    for i in range(1,10):
+        if ways + combs(length-2) >= n:
+            return str(i) + solve(length-2) + str(10-i)
+        ways += combs(length - 2)
+
+n = int(input())
+ways = 0
+l = 0
+while ways < n:
+    l += 1
+    ways += combs(l)
+ways -= combs(l)
+print(solve(l))
