8ef6567a9a5f49e970b87ac85bc9a85291eddae0
RedMincrafter <88317245+RedMincrafter@users.noreply.github.com>
2024-09-02 02:30:25 +0100

    Update index.tsx
    more editorial cus fun

--- solutions/bio1/Ants/index.tsx
+++ solutions/bio1/Ants/index.tsx
@@ -33,6 +33,21 @@ export const Ants = () => {
         If an ant falls off the edge of the board, you can set a flag, which prevents it from running any more functions, and prints "Removed" instead of its position.
       </SText>
 
+      <SSubtitle>Initialising the board</SSubtitle>
+      <SText>
+        Initalising the board is simple: an $11$x$11$ white grid. (I used a 2D Boolean List.)
+        Initalising the ants is also simple, input their positions and directions, and then parse them into usable data. (I subtract one for zero indexing, and change the direction into a number, which i use as an index to a list of directions.
+      </SText>
+
+      <SSubtitle>Mainloop</SSubtitle>
+      <SText>
+        Before the mainloop, the program takes in an initial value for $n$, then uses it in a while loop, which has the condition ($n$!=-1).
+        This allows it to exit immediately on the first round, rather than producing buggy behaviour.
+        After this, both bugs are moved in turn $n$ times.
+        The entire grid is then printed out.
+        Finally, both bugs' positions are printed out, checking if they have fallen off.
+        At the end of the mainloop, a new value for $n$ is taken in, to repeat the cycle.
+      </SText>
       <STitle>Code</STitle>
       <SCodeBlock path="ants/sol"/>
     </>
