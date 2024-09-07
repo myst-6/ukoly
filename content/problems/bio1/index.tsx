9674def103c1daa3c4246bc562eff28c90e196f0
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-09-07 19:51:44 +0100

    Update index.tsx

--- content/problems/bio1/index.tsx
+++ content/problems/bio1/index.tsx
@@ -63,6 +63,7 @@ import {
   Shuffling,
   FourInALine,
   DieTipping,
+  PuzzleGame,
 } from "solutions";
 
 export const bio1Problems: BIO1ProblemInfo[] = [
@@ -747,5 +748,16 @@ export const bio1Problems: BIO1ProblemInfo[] = [
     editorialAuthors: ["Adwaya Gupta"],
     solutionAuthors: ["Adwaya Gupta"],
     component: <DieTipping />,
+  },
+  {
+    difficulty: "Hard",
+    display: "Puzzle Game",
+    original: "https://www.olympiad.org.uk/papers/2009/bio/bio09-exam.pdf",
+    year: 2009,
+    question: 2,
+    tags: ["Implementation", "DFS and Similar"],
+    editorialAuthors: ["Adwaya Gupta"],
+    solutionAuthors: ["Adwaya Gupta"],
+    component: <PuzzleGame />,
   },
 ];
