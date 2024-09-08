d195d52276cc0fd38204ba8bbb9b9944f8ffb9e6
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-09-08 08:59:10 +0100

    Create index.tsx

---
+++ solutions/bio1/MorseCode/index.tsx
@@ -0,0 +1,26 @@
+import { SCodeBlock, SText, STitle } from "components";
+
+export const MorseCode = () => {
+  return (
+    <>
+      <STitle>Idea</STitle>
+      <SText>
+        Let's first notice the small constraint on the size of the word.
+        This suggests we can try every possible valid split of the word.
+      </SText>
+
+      <STitle>Solution</STitle>
+      <SText>
+        We can solve this problem by using a recursive function that tries every possible split of the morse code.
+        For each split, we can check if the first part is a valid morse code character.
+        If it is, we can recurse on the rest of the word.
+        If we reach the end of the word, we have found a valid split.
+        We should ensure we do not perform more than $s$ splits, where $s$ is the size of the original word.
+        To prevent duplicate work, we can memoize the results of the recursive function.
+      </SText>
+
+      <STitle>Code</STitle>
+      <SCodeBlock path="morsecode/sol"/>
+    </>
+  );
+};
