adbf59959fa6a01b1f1e99eac6f89d8c5306e44e
Yuvan Raja <yrvp5184@gmail.com>
2024-09-09 19:58:44 +0100

    Fixed LaTeX

--- solutions/bio1/AlphaComplex/index.tsx
+++ solutions/bio1/AlphaComplex/index.tsx
@@ -8,7 +8,7 @@ export const AlphaComplex = () => {
         Since the constraints are reasonably low, we will simply implement the procedure as described in the question. Firstly we will construct the edges of the rooms, storing these as adjacency lists. We can then ouput these adjacency lists. 
       </SText>
       <SText>
-        Next, we will store two arrays indicating whether each exit, and each room has been passed through an odd or even number of times. Using this information we can decide which room to walk to, q times.
+        Next, we will store two arrays indicating whether each exit, and each room has been passed through an odd or even number of times. Using this information we can simply decide which room to walk to next, $q$ times.
       </SText>
       <SCodeBlock path="alphacomplex/sol"/>
     </>
