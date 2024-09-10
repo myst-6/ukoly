a5d49bf7571bef249ecb7b02cb0f8f8daad267de
yuvan <101983248+ykr5184@users.noreply.github.com>
2024-09-10 17:51:14 +0100

    Typo Fix

--- solutions/bio1/AlphaComplex/index.tsx
+++ solutions/bio1/AlphaComplex/index.tsx
@@ -5,7 +5,7 @@ export const AlphaComplex = () => {
     <>
       <STitle>Solution</STitle>
       <SText>
-        Since the constraints are reasonably low, we will simply implement the procedure as described in the question. Firstly we will construct the edges of the rooms, storing these as adjacency lists. We can then ouput these adjacency lists. 
+        Since the constraints are reasonably low, we will simply implement the procedure as described in the question. Firstly we will construct the edges of the rooms, storing these as adjacency lists. We can then output these adjacency lists. 
       </SText>
       <SText>
         Next, we will store two arrays indicating whether each exit, and each room has been passed through an odd or even number of times. Using this information we can simply decide which room to walk to next, $q$ times.
