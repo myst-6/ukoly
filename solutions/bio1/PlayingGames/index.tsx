13a607ab0f4b772255c23b395cf41fe21ab7ce68
RedMincrafter <88317245+RedMincrafter@users.noreply.github.com>
2024-09-10 21:15:39 +0100

    Update index.tsx
    i removed everything

--- solutions/bio1/PlayingGames/index.tsx
+++ solutions/bio1/PlayingGames/index.tsx
@@ -1,4 +1,4 @@
-import { SCodeBlock, SText, STitle, SCode } from "components";
+import { SCodeBlock, SText, STitle, SCode, SList } from "components";
 
 export const PlayingGames = () => {
   return (
@@ -8,23 +8,27 @@ export const PlayingGames = () => {
         Given the first two dice, you can generate every single combination, in a set.
         Given the first die of the second pair, you can determine whether the dice will produce an identical distribution, and if so, you can find the last die.
       </SText>
-      <STitle>Brute Force</STitle>
+      <STitle>Brute Force Solution</STitle>
       <SText>
-        We can brute force every possible first die, as we know the first pair of dice have a maximum value of $8$, the maximum possible combination is $16$,
-        and we know that the largest face of at least one of the second pair is less than or equal to
-        $8$, as the largest face of both cannot be greater than $8$, as this would mean their sum is more than or equal to $18$, which is too big.
-        To brute force the first die of the second pair, iterate through all the numbers from one to eight for all faces, and dont let a face value be smaller than one before it (to avoid duplicates).
-        With the maximum possible $n$ being $8$, we can just have $8$ nested for loops, to generate every single die face. However for $n \lt 8$, we can set the for loops to not iterate, and ignore the first $8-n$ faces.
-        Note that there is a Python module which does this for us, <SCode> itertools.combinations_with_replacement </SCode> which brute forces the first die as described.
+        Let the input dice be $A$ and $B$, and the output dice be $C$ and $D$.
+        We begin by producing the distribution of $A$ and $B$.
+        Pairwise add each face of $A$ to each face of $B$, and then store them in a container - I used a <SCode>Counter</SCode>.
+        We brute force over all possible $C$ using Python's <SCode> itertools.combinations_with_replacement </SCode> function, or an equivalent implementation in another language.
+        We then iterate to generate each element of $D$. This takes $n$ steps.
       </SText>
-  
-      <STitle>Solution</STitle>
+      <SText>At the $i$-th step:</SText>
+      <SList>
+        <SText>Pop the smallest element from the distribution.</SText>
+        <SText>The $i$-th element of $D$ is this element $-$ the first element of $C$.</SText>
+        <SText>If this is less than 1, we can early return as all dice faces must be $\ge 1$</SText>
+        <SText>For the remaining elements of $C$, check if adding them to the $i$-th element of $D$ provides a valid value from the distribution and pop it.</SText>
+        <SText>If this value was not in the distribution, we can do another early return.</SText>
+      </SList>
       <SText>
-        We brute force over all possible first die as described above.
-        We then check if a second die exists.
-        The first face of the second die is simply the smallest combination minus the first face of the first die.
-        We then check if the other faces provide valid combinations.
-        Repeat this for all the faces, and if everything is valid, it will provide an answer.
+        If no early returns happened, check that $C$ is not equal to $A$ or $B$, as these are not valid.
+        If so, print $C$ and $D$ and terminate the program.
+        If an early return has happened - continue with the next value of $C$.
+        If every possible $C$ has been tested, then it is impossible.
       </SText>
   
       <STitle>Code</STitle>
