fe24399959efb65f9829cc5d5c3945ce2dea26a8
RedMincrafter <88317245+RedMincrafter@users.noreply.github.com>
2024-09-10 20:35:36 +0100

    Update index.tsx

--- solutions/bio1/PlayingGames/index.tsx
+++ solutions/bio1/PlayingGames/index.tsx
@@ -11,17 +11,17 @@ export const PlayingGames = () => {
       <STitle>Brute Force</STitle>
       <SText>
         We can brute force every possible first die, as we know the first pair of dice have a maximum value of $8$, the maximum possible combination is $16$, and we know that at least one of the second pair
-        has a value of less than or equal to $8$ , as the biggest value of both cannot be greater than $8$ , as this would mean their sum is more than or equal to $18$, which is too big
+        has a value of less than or equal to $8$, as the biggest value of both cannot be greater than $8$, as this would mean their sum is more than or equal to $18$, which is too big
         To brute force the first die of the second pair, iterate through all the numbers from one to eight for all faces, and dont let a face value be smaller than one before it (to avoid duplicates).
-        With the maximum possible $n$ being $8$, we can just have $8$ nested for loops, to generate every single die face. However for $n$ less than $8$, we can set the for loops to not iterate, and ignore the first $8-n$ faces.
-        Note that there is a python module which does this for us, <SCode> itertools.combinations_with_replacement </SCode> which brute forces the first die as described.
+        With the maximum possible $n$ being $8$, we can just have $8$ nested for loops, to generate every single die face. However for $n \lt 8$, we can set the for loops to not iterate, and ignore the first $8-n$ faces.
+        Note that there is a Python module which does this for us, <SCode> itertools.combinations_with_replacement </SCode> which brute forces the first die as described.
       </SText>
   
       <STitle>Solution</STitle>
       <SText>
         We brute force over all possible first die as described above.
         We then check if a second die exists.
-        The first face of the second die is simply the smallest combinatition minus the first face of the first die.
+        The first face of the second die is simply the smallest combination minus the first face of the first die.
         We then check if the other faces provide valid combinations.
         Repeat this for all the faces, and if everything is valid, it will provide an answer.
       </SText>
