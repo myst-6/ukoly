585003ae14ff86d64d93c14e7aec7bdcb1053249
Alex Pylypenko <macaquedev@gmail.com>
2024-09-14 13:09:17 +0100

    Changed problem selector

--- pages/grader.tsx
+++ pages/grader.tsx
@@ -1,48 +1,43 @@
 import { Box, Header, HStack } from "components";
 import { useColorMode, Text, Menu, MenuButton, Button, MenuItem, MenuList } from "@chakra-ui/react";
-
+import { BIO1ProblemInfo, bio1Problems } from "content";
 import { pages } from "content";
 import { useRef, useState } from "react";
 import { Editor } from "@monaco-editor/react";
 
-const langs = {
-    python: "Python", 
-    cpp: "C++",
-}
-const years = Array.from({ length: 25 }, (_, i) => 2000 + i).reduce((acc, year) => {
-    acc[year] = year;
-    return acc;
+const years = bio1Problems.reduce((acc: Record<number, number>, problem) => {
+  acc[problem.year] = problem.year;
+  return acc;
 }, {} as Record<number, number>);
 
-const questions = {
-    q1: "1",
-    q2: "2",
-    q3: "3"
+const langs = {
+  python: "Python",
+  cpp: "C++",
 }
 
-const Selector = ({name, opts, opt, onSelect}: any) => {
-    return <Box display="flex" alignItems="center">
-        <Text ml={2} fontSize="lg" padding="1%">{name}: </Text>
-        <Menu>
-            <MenuButton as={Button}>{opts[opt]}</MenuButton>
-            <MenuList maxHeight="20vh" overflowY="auto">
-                {Object.entries(opts).map(([key, label]:any) => {
-                    return <MenuItem key={key} onClick={()=>onSelect(key)}>{label}</MenuItem>
-                })}
-            </MenuList>
-        </Menu>
-    </Box>
+const Selector = ({ name, opts, opt, onSelect }: any) => {
+  return <Box display="flex" alignItems="center">
+    <Text ml={2} fontSize="lg" padding="1%">{name}: </Text>
+    <Menu>
+      <MenuButton as={Button}>{opts[opt]}</MenuButton>
+      <MenuList maxHeight="20vh" overflowY="auto">
+        {Object.entries(opts).map(([key, label]: any) => {
+          return <MenuItem key={key} onClick={() => onSelect(key)}>{label}</MenuItem>
+        })}
+      </MenuList>
+    </Menu>
+  </Box>
 }
 
 export default function grader() {
   const editorRef = useRef();
-  const {colorMode} = useColorMode();
+  const { colorMode } = useColorMode();
 
 
   const [value, setValue] = useState('');
   const [language, setLanguage] = useState<keyof typeof langs>('python')
   const [year, setYear] = useState<keyof typeof years>(2024)
-  const [q, setq] = useState<keyof typeof questions>("q1")
+  const [q, setq] = useState<number>(0)
 
 
   const onMount = (editor: any) => {
@@ -58,7 +53,13 @@ export default function grader() {
           <Selector name={"Language"} opts={langs} opt={language} onSelect={setLanguage}/>
           <HStack>
             <Selector name={"Year"} opts={years} opt={year} onSelect={setYear}/>
-            <Selector name={"Question"} opts={questions} opt={q} onSelect={setq}/>
+            <Selector name={"Question"} opts={
+              bio1Problems.filter((problem: BIO1ProblemInfo) => problem.year == year) // implicit type conversion, I hate javascript!
+              .reduce((acc: Record<number, string>, problem) => {
+                acc[problem.question] = `${problem.question}. ${problem.display}`;
+                return acc;
+              }, {} as Record<number, string>)
+            } opt={q} onSelect={setq}/>
           </HStack>
         </HStack>
         <Editor
