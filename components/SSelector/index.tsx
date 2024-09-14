c0902631454f5fa3082c6db5eea06f0bf3665302
Alex Pylypenko <macaquedev@gmail.com>
2024-09-14 23:56:47 +0100

    Fixed buttons

--- components/SSelector/index.tsx
+++ components/SSelector/index.tsx
@@ -4,7 +4,7 @@ import { Box } from "components";
 export const SSelector = ({name, opts, opt, onSelect}: any) => {
     return (
         <Box display="flex" alignItems="center">
-            <Text ml={2} fontSize="lg" padding="1%">{name}: </Text>
+            <Text ml={2} fontSize="lg" padding="0.75em">{name}: </Text>
             <Menu>
                 <MenuButton as={Button}>{opts[opt]}</MenuButton>
                 <MenuList maxHeight="20vh" overflowY="auto">
