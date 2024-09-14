3d35a90d8719406c69b0467d84ff1e27cf2b4652
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-09-14 18:13:54 +0100

    Update CodeBlock.tsx

--- components/SCodeBlock/CodeBlock.tsx
+++ components/SCodeBlock/CodeBlock.tsx
@@ -1,48 +1,108 @@
-import { CSSProperties } from "react";
-import SyntaxHighlighter from "react-syntax-highlighter";
-import { FaRegCopy } from "react-icons/fa";
-import { Box, type BoxProps } from "../Box";
-import { IconButton } from "../IconButton";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon, Tab, TabList, TabPanel, TabPanels, Tabs, 
+  Box
+} from "@chakra-ui/react";
+import { useEffect, useState } from "react";
+import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
+import { CodeBlock } from "./CodeBlock";
+import { SRunner, languages } from "components";
 
-export interface Theme {
-  [key: string]: CSSProperties;
+export const base: string = "/assets/code/";
+
+/*
+
+See here for a list of highlights:
+https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/HEAD/AVAILABLE_LANGUAGES_HLJS.MD
+
+*/
+
+
+export type SourceCode = null | string;
+
+export interface SCodeBlockProps {
+  path: string;
 }
 
-export interface CodeBlockProps extends BoxProps {
-  code: string;
-  language: string;
-  theme: Theme;
-}
+export const SCodeBlock = ({ path }: SCodeBlockProps) => {
+  const [codes, setCodes] = useState<SourceCode[]>(languages.map(() => null));
 
-export const CodeBlock = ({ code, language, theme }: CodeBlockProps) => {
+  useEffect(() => {
+    async function getCode(ext: string): Promise<SourceCode> {
+      try {
+        const res = await fetch(`${base}${path}.${ext}`);
+        if (res.status === 404) {
+          // file with this extension not found, no problem!
+          return null;
+        } else {
+          return res.text();
+        }
+      } catch (error) {
+        // some severe error
+        console.error("Sorry, there was an error fetching the solution.");
+        console.error("Please report this to Boris on discord.");
+        console.error(error);
+        return null;
+      }
+    }
+    (async () => {
+      const codes = await Promise.all(languages.map(({ extension }) => getCode(extension)));
+      setCodes(codes);
+    })();
+  }, [path]);
+
   return (
-    <Box >
-      <IconButton
-        aria-label="Copy Code" 
-        icon={<FaRegCopy />} 
-        style={{
-          position: "absolute",
-          right: 64,
-          marginTop: 16
-        }}
-        onClick={() => navigator.clipboard.writeText(code)}
-      />
-      <SyntaxHighlighter
-        language={language}
-        wrapLines={false}
-        wrapLongLines={false}
-        showLineNumbers={true}
-        showInlineLineNumbers={false}
-        style={theme}
-        customStyle={{
-          border: "1px solid #c3c3c3",
-          borderRadius: "5px",
-          maxHeight: "30em",
-          overflowY: "scroll",
-        }}
-      >
-        {code}
-      </SyntaxHighlighter>
-    </Box>
-  );
+    <>
+      <Accordion defaultIndex={[1]} allowMultiple>
+        <AccordionItem>
+          <AccordionButton>
+            <Box as='span' flex='1' textAlign='left'>
+              Click to see code:
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel>
+            <Tabs isFitted defaultIndex={0} variant="line">
+              <TabList mb={1}>
+                {
+                  ...languages.filter((_, idx) => codes[idx] !== null).map(({ display, extension }) => {
+                    return <Tab key={extension}>{display}</Tab>
+                  })
+                }
+              </TabList>
+              <TabPanels>
+                {
+                  ...Object.entries(codes).filter(([_, code]) => code !== null).map(([idx, code]) => {
+                    return (
+                      <TabPanel key={+idx}>
+                        <CodeBlock
+                          theme={atomOneDark}
+                          code={code!}
+                          language={languages[+idx]!.highlight}
+                        />
+                      </TabPanel>
+                    )
+                  })
+                }
+              </TabPanels>
+            </Tabs>
+          </AccordionPanel>
+        </AccordionItem>
+        <AccordionItem>
+          <AccordionButton>
+            <Box as='span' flex='1' textAlign='left'>
+              Click to test on custom inputs:
+            </Box>
+            <AccordionIcon />
+          </AccordionButton>
+          <AccordionPanel>
+            <SRunner codes={codes} />
+          </AccordionPanel>
+        </AccordionItem>
+      </Accordion>
+    </>
+  )
 }
