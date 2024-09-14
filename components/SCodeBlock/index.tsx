0100ce25fa07082086379848d70182ea007b79f0
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-09-14 18:16:40 +0100

    Update index.tsx

--- components/SCodeBlock/index.tsx
+++ components/SCodeBlock/index.tsx
@@ -1,208 +1,48 @@
-import {
-  Accordion,
-  AccordionItem,
-  AccordionButton,
-  AccordionPanel,
-  AccordionIcon, Tab, TabList, TabPanel, TabPanels, Tabs, Textarea,
-  HStack,
-  VStack,
-  Button,
-  Flex,
-  Box
-} from "@chakra-ui/react";
-import { useEffect, useState } from "react";
-import { atomOneDark } from "react-syntax-highlighter/dist/cjs/styles/hljs";
-import { CodeBlock } from "./CodeBlock";
-import { STitle } from "components";
-import { useRunner } from "utils";
+import { CSSProperties } from "react";
+import SyntaxHighlighter from "react-syntax-highlighter";
+import { FaRegCopy } from "react-icons/fa";
+import { Box, type BoxProps } from "../Box";
+import { IconButton } from "../IconButton";
 
-export const base: string = "/assets/code/";
-
-export interface Language {
-  display: string;
-  extension: string;
-  highlight: string;
-  pistonName: string;
-  monaco: string;
-  version: string;
-  template: string;
-  initPos: { lineNumber: number, column: number };
+export interface Theme {
+  [key: string]: CSSProperties;
 }
 
-/*
-
-See here for a list of highlights:
-https://github.com/react-syntax-highlighter/react-syntax-highlighter/blob/HEAD/AVAILABLE_LANGUAGES_HLJS.MD
-
-*/
-
-export const languages: Language[] = [
-  {
-    display: "C++",
-    extension: "cpp",
-    highlight: "cpp",
-    pistonName: "c++",
-    monaco: "cpp",
-    version: "10.2.0",
-    template: `#include <bits/stdc++.h>
-using namespace std;
-
-int main() {
-    // Enter your solution below. 
-    // Use std::cin and std::cout for I/O, and do not 
-    // output any prompts, because the grader will not work.
-    
-    
-    
-    return 0;
-}`,
-    initPos: { lineNumber: 9, column: 5 }
-  },
-  {
-    display: "Python",
-    extension: "py",
-    highlight: "python",
-    pistonName: "python",
-    monaco: "python",
-    version: "3.10",
-    template: `# Enter your solution below. Use input() 
-# and print() for I/O, and do not output
-# any prompts, because the grader will not work.
-
-`,
-    initPos: { lineNumber: 5, column: 1 }
-  },
-];
-
-export type SourceCode = null | string;
-export type TestCase = null | string;
-export type Output = null | string;
-
-export interface SCodeBlockProps {
-  path: string;
+export interface CodeBlockProps extends BoxProps {
+  code: string;
+  language: string;
+  theme: Theme;
 }
 
-export const SCodeBlock = ({ path }: SCodeBlockProps) => {
-  const [codes, setCodes] = useState<SourceCode[]>(languages.map(() => null));
-  const [testCase, setTestCase] = useState<TestCase>("");
-  const [output, setOutput] = useState<Output>(null);
-
-  useEffect(() => {
-    async function getCode(ext: string): Promise<SourceCode> {
-      try {
-        const res = await fetch(`${base}${path}.${ext}`);
-        if (res.status === 404) {
-          // file with this extension not found, no problem!
-          return null;
-        } else {
-          return res.text();
-        }
-      } catch (error) {
-        // some severe error
-        console.error("Sorry, there was an error fetching the solution.");
-        console.error("Please report this to Boris on discord.");
-        console.error(error);
-        return null;
-      }
-    }
-    (async () => {
-      const codes = await Promise.all(languages.map(({ extension }) => getCode(extension)));
-      setCodes(codes);
-    })();
-  }, [path]);
-
-  const { dispatch, results } = useRunner();
-  
-  const handleRunCode = () => {
-    const idx = codes.findIndex(code => code !== null);
-    dispatch([testCase ?? ""], codes[idx]!, languages[idx]!);
-  };
-
-  useEffect(() => {
-    if (results.length === 0) return;
-    const { status, message } = results[0]!;
-    if (status === "TS") {
-      setOutput("Waiting...");
-    } else {
-      setOutput(message);
-    }
-  }, [results]);
-
+export const CodeBlock = ({ code, language, theme }: CodeBlockProps) => {
   return (
-    <>
-      <Accordion defaultIndex={[1]} allowMultiple>
-        <AccordionItem>
-          <AccordionButton>
-            <Box as='span' flex='1' textAlign='left'>
-              Click to see code:
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-          <AccordionPanel>
-            <Tabs isFitted defaultIndex={0} variant="line">
-              <TabList mb={1}>
-                {
-                  ...languages.filter((_, idx) => codes[idx] !== null).map(({ display, extension }) => {
-                    return <Tab key={extension}>{display}</Tab>
-                  })
-                }
-              </TabList>
-              <TabPanels>
-                {
-                  ...Object.entries(codes).filter(([_, code]) => code !== null).map(([idx, code]) => {
-                    return (
-                      <TabPanel key={+idx}>
-                        <CodeBlock
-                          theme={atomOneDark}
-                          code={code!}
-                          language={languages[+idx]!.highlight}
-                        />
-                      </TabPanel>
-                    )
-                  })
-                }
-              </TabPanels>
-            </Tabs>
-          </AccordionPanel>
-        </AccordionItem>
-        <AccordionItem>
-          <AccordionButton>
-            <Box as='span' flex='1' textAlign='left'>
-              Click to test on custom inputs:
-            </Box>
-            <AccordionIcon />
-          </AccordionButton>
-          <AccordionPanel>
-            <HStack justifyContent="center" alignItems="center" width="100%">
-              <VStack flex="1" spacing={4}>
-                <STitle size="sm">Input</STitle>
-                <Textarea
-                  placeholder="Enter a test case: "
-                  size="md"
-                  width="100%"
-                  value={testCase ?? ""}
-                  onChange={(e) => setTestCase(e.target.value)}
-                />
-              </VStack>
-              <VStack flex="1" spacing={4}>
-                <STitle size="sm">Output</STitle>
-                <Textarea
-                  placeholder="Output will appear here"
-                  size="md"
-                  width="100%"
-                  isReadOnly
-                  value={output ?? ""}
-                />
-              </VStack>
-            </HStack>
-            <Flex justifyContent="center" width="100%" marginTop="15px">
-              <Button colorScheme="teal" size="md" onClick={handleRunCode}>
-                Run Code
-              </Button>
-            </Flex>
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
-    </>
-  )
+    <Box >
+      <IconButton
+        aria-label="Copy Code" 
+        icon={<FaRegCopy />} 
+        style={{
+          position: "absolute",
+          right: 64,
+          marginTop: 16
+        }}
+        onClick={() => navigator.clipboard.writeText(code)}
+      />
+      <SyntaxHighlighter
+        language={language}
+        wrapLines={false}
+        wrapLongLines={false}
+        showLineNumbers={true}
+        showInlineLineNumbers={false}
+        style={theme}
+        customStyle={{
+          border: "1px solid #c3c3c3",
+          borderRadius: "5px",
+          maxHeight: "30em",
+          overflowY: "scroll",
+        }}
+      >
+        {code}
+      </SyntaxHighlighter>
+    </Box>
+  );
 }
