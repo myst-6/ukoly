6dce866ac05b9ac2a2c7fa82ef843a3fb45587ba
myst-6 <michaelbjhall@gmail.com>
2024-09-16 17:49:14 +0100

    uselayouteffect => useeffect

--- utils/dimensions/useWindowDimensions.tsx
+++ utils/dimensions/useWindowDimensions.tsx
@@ -1,10 +1,10 @@
-import { useLayoutEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Dimensions, empty } from "./base";
 
 export function useWindowDimensions() {
   const [dimensions, setDimensions] = useState<Dimensions>(empty);
 
-  useLayoutEffect(() => {
+  useEffect(() => {
     function update() {
       const { innerWidth: width, innerHeight: height } = window;
       console.log("window", {width,height});
