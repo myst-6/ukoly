b8cb974a4ab44ff34a9a4fdec6aa64b6949506e4
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2024-09-19 21:20:32 +0100

    Create Pentominoes.tsx

---
+++ content/tests/Pentominoes.tsx
@@ -0,0 +1,64 @@
+import { Test } from './base';
+
+export const TestPentominoes: Test[] = [
+    {
+        input: "FF",
+        output: "7",
+        points: 1
+    },
+    {
+        input: "XX",
+        output: "6",
+        points: 2
+    },
+    {
+        input: "TT",
+        output: "8",
+        points: 2
+    },
+    {
+        input: "II",
+        output: "10",
+        points: 2
+    },
+    {
+        input: "GX",
+        output: "13",
+        points: 2
+    },
+    {
+        input: "QP",
+        output: "14",
+        points: 2
+    },
+    {
+        input: "MW",
+        output: "15",
+        points: 2
+    },
+    {
+        input: "ZY",
+        output: "16",
+        points: 2
+    },
+    {
+        input: "NU",
+        output: "16",
+        points: 2
+    },
+    {
+        input: "AS",
+        output: "16",
+        points: 2
+    },
+    {
+        input: "JL",
+        output: "18",
+        points: 2
+    },
+    {
+        input: "VI",
+        output: "19",
+        points: 2
+    }
+];
