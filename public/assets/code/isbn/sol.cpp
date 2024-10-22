13111969fab576bdd93dbe4e29742786bf545759
Alex Pylypenko <macaquedev@gmail.com>
2024-10-23 00:17:26 +0100

    Fixed minor bloat

--- public/assets/code/isbn/sol.cpp
+++ public/assets/code/isbn/sol.cpp
@@ -1,44 +1,33 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-int main()
-{
-    string input;
-    cin >> input;
+int main() {
+    string s;
+    cin >> s;
+    
+    int idx = s.find('?');
+    int multiplier = 10 - idx;
+    int total = 0;
 
-    vector<int> digits(10);
-    int question_mark = 0;
-    for (int i = 0; i < 10; i++)
-    {
-        if (input[i] == 'X')
-        {
-            digits[i] = 10;
-        }
-        else if (input[i] == '?')
-        {
-            digits[i] = -1;
-            question_mark = i;
-        }
-        else
-        {
-            digits[i] = input[i] - '0';
+    for (int i = 0; i < s.size(); ++i) {
+        if (s[i] != '?') {
+            int val;
+            if (s[i] == 'X') {
+                val = 10;
+            } else {
+                val = s[i] - '0';  // convert character to integer
+            }
+            total += (10 - i) * val;
         }
     }
 
-    for (int digit = 0; digit < 10; digit++)
-    {
-        digits[question_mark] = digit;
-
-        int sum = 0;
-        for (int i = 0; i < 10; i++)
-        {
-            sum += digits[i] * (10 - i);
-        }
-
-        if (sum % 11 == 0)
-        {
-            cout << digit << endl;
+    for (int i = 0; i <= 10 + (idx == 9); ++i) {
+        if ((total + i * multiplier) % 11 == 0) {
+            if (i == 10) {
+                cout << 'X' << endl;
+            } else {
+                cout << i << endl;
+            }
             break;
         }
     }
