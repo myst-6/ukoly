4e55274a1cafcca01921ff7d8445e4bfecb26be5
Alex Pylypenko <macaquedev@gmail.com>
2024-10-27 22:50:50 +0000

    Fixed

--- public/assets/code/dreamingspires/sol.py
+++ public/assets/code/dreamingspires/sol.py
@@ -1,37 +1,46 @@
 # Solution author: Alex Pylypenko
+
 from collections import deque
-from string import ascii_uppercase
 
 
-def add(s):
-    return s + ascii_uppercase[len(s)]
+def solve(start, end):
+    start_state = start.split()
+    end_state = end.split()
+    if start_state == end_state:
+        return 0
+    start_state = [list(i) for i in start_state]
+    end_state = [list(i) for i in end_state]
 
-def swap(s):
-    return s[1] + s[0] + s[2:]
+    for i in range(4):
+        if start_state[i] == ['0']:
+            start_state[i] = []
+        if end_state[i] == ['0']:
+            end_state[i] = []
 
-def rotate(s):
-    return s[1:] + s[0]
-
-
-def solve(s: str) -> int:
-    bfs = deque([("", 0)])
-    visited = {""}
-    while bfs:
-        current, steps = bfs.popleft()
-        if current == s:
-            return steps
-        if len(current) < len(s):
-            new_string = add(current)
-            if new_string not in visited:
-                visited.add(new_string)
-                bfs.append((new_string, steps + 1))
-        if len(current) > 1:
-            for new_string in [swap(current), rotate(current)]:
-                if new_string not in visited:
-                    visited.add(new_string)
-                    bfs.append((new_string, steps + 1))
+    q = deque([(0, tuple(tuple(i) for i in start_state))])
+    seen = set()
+    while q:
+        distance, state = q.popleft()
+        if state in seen:
+            continue
+        seen.add(state)
+        for old in range(4):
+            if not state[old]:
+                continue
+            new_state = [list(i) for i in state]
+            element = new_state[old].pop()
+            for new in range(4):
+                if new == old:
+                    continue
+                ns = new_state[:][:]
+                ns[new].append(element)
+                if ns == end_state:
+                    return distance + 1
+                q.append((distance + 1, tuple(tuple(i) for i in ns)))
+                ns[new].pop()
 
 
 if __name__ == "__main__":
-    s = input()
-    print(solve(s))
+    a = input()
+    b = input()
+    print(solve(a, b))
