9e0107b5255570e342736a4f0c676d2acdb2927d
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2025-04-27 19:02:53 +0100

    Create ShortFuse.tsx

---
+++ content/tests/ShortFuse.tsx
@@ -0,0 +1,69 @@
+import { Test } from './base';
+
+export const TestShortFuse: Test[] = [
+    {
+        input: "2\n1 1",
+        output: "7",
+        points: 1
+    },
+    {
+        input: "1\n1",
+        output: "3",
+        points: 2
+    },
+    {
+        input: "2\n2 4",
+        output: "9",
+        points: 2
+    },
+    {
+        input: "2\n1 3",
+        output: "11",
+        points: 2
+    },
+    {
+        input: "3\n100 100 100",
+        output: "16",
+        points: 2
+    },
+    {
+        input: "3\n7 14 14",
+        output: "24",
+        points: 2
+    },
+    {
+        input: "3\n16 32 128",
+        output: "45",
+        points: 2
+    },
+    {
+        input: "3\n10 11 12",
+        output: "89",
+        points: 2
+    },
+    {
+        input: "3\n43 401 4757",
+        output: "101",
+        points: 2
+    },
+    {
+        input: "4\n1 1 1 1",
+        output: "35",
+        points: 2
+    },
+    {
+        input: "4\n1 2 4 8",
+        output: "133",
+        points: 2
+    },
+    {
+        input: "4\n5 41 83 137",
+        output: "891",
+        points: 2
+    },
+    {
+        input: "4\n1483 1801 3407 4999",
+        output: "1781",
+        points: 2
+    }
+];
