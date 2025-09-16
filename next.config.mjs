236adf172b97c4e8b10e36071598c35982c322a6
myst-6 <mborishall@gmail.com>
2025-09-16 20:16:12 +0100

    exclude api-worker from the next build

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/api-worker/**']
+    };
+    return config;
+  },
+  experimental: {
+    outputFileTracingExcludes: {
+      '*': ['./api-worker/**/*']
+    }
+  }
 };
 
 export default nextConfig;
