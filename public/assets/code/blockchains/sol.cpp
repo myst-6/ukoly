14cfc53230b3bdb334269b19b7d7bebb85d3d3d3
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2026-01-14 21:29:29 +0000

    byebye gippity

--- public/assets/code/blockchains/sol.cpp
+++ public/assets/code/blockchains/sol.cpp
@@ -1,95 +1,98 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 #include <unordered_map>
 #include <string>
-#include <cstring>
+#include <unordered_set>
 
 using namespace std;
 
 unordered_map<char, int> conversion;
 vector<int> order;
 int n;
-int memo[(1 << 18)][19][19];
+int memo[(1 << 18)][18][18];
 
 pair<int, pair<int, int>> CheckPrefix(vector<int> order) {
 
-    int a = n; 
-    int b = n;
-
-    for (int j : order) {
-        if (j > b) {
-            return {1, {0, 0}};
-        }
-        if (j > a) {
-            b = j;
-        } else {
-            a = j;
-        }
-    }
-    return {0, {a, b}};
+	int a = 26; int b = 26;
+	for (int i = 0; i < order.size(); i++) {
+		int j = order[i];
+		if (j > b) {
+			return { 1, {0, 0} };
+		}
+		if (j > a) {
+			b = j;
+		}
+		else {
+			a = j;
+		}
+	}
+	return { 0, {a, b} };
 }
 
+
 int Construct(int remaining, int a, int b) {
+	if (a > b) return 0;
 
-    if (a > b) return 0;
+	if (remaining == 0) {
+		return 1;
+	}
 
-    if (remaining == 0) {
-        return 1;
-    }
+	int total = 0;
 
-    int &res = memo[remaining][a][b];
-    if (res != -1) return res;
+	if (memo[remaining][a][b] != 0) {
+		return memo[remaining][a][b] - 1;
+	}
 
-    res = 0;
 
-    for (int element = 0; element < n; element++) {
+	for (int element = 0; element < n; element++) {
+		int next_a = a; int next_b = b;
+		if ((remaining & (1 << element)) == 0) {
+			continue;
+		}
+		if (element > b) {
+			continue;
+		}
+		else if (element > a) {
+			next_b = element;
+		}
+		else {
+			next_a = element;
+		}
 
-        if ((remaining & (1 << element)) == 0) continue;
-        if (element > b) continue;
+		total += Construct(remaining - (1 << element), next_a, next_b);
+	}
 
-        int next_a = a;
-        int next_b = b;
-
-        if (element > a) {
-            next_b = element;
-        } else {
-            next_a = element;
-        }
-
-        res += Construct(remaining ^ (1 << element), next_a, next_b);
-    }
-
-    return res;
+	memo[remaining][a][b] = total + 1;
+	return total;
 }
 
 
 int main() {
 
-    memset(memo, -1, sizeof(memo));
+	string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	for (int i = 0; i < 26; i++) {
+		conversion[letters[i]] = i;
+	}
+	cin >> n;
 
-    string letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    for (int i = 0; i < 26; i++) {
-        conversion[letters[i]] = i;
-    }
+	int remaining = (1 << n) - 1;
 
-    cin >> n;
-    int remaining = (1 << n) - 1;
+	string prefix; cin >> prefix;
 
-    string prefix;
-    cin >> prefix;
+	for (int i = 0; i < prefix.size(); i++) {
+		order.push_back(conversion[prefix[i]]);
+		remaining = remaining - (1 << conversion[prefix[i]]);
+	}
 
-    for (char c : prefix) {
-        int idx = conversion[c];
-        order.push_back(idx);
-        remaining ^= (1 << idx);
-    }
+	pair<int, pair<int, int>> Checker = CheckPrefix(order);
 
-    auto Checker = CheckPrefix(order);
+	if (Checker.first == 1) {
+		cout << 0;
+		return 0;
+	}
 
-    if (Checker.first == 1) {
-        cout << 0;
-        return 0;
-    }
+	int total = Construct(remaining, Checker.second.first, Checker.second.second);
+	cout << total;
 
-    cout << Construct(remaining, Checker.second.first, Checker.second.second);
 }
