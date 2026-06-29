57e282d033d8eeac8134a4b7b45d6b2970f17b0b
AG-88301 <69068329+AG-88301@users.noreply.github.com>
2026-06-29 15:45:27 +0100

    sols
    someone write editorials

---
+++ solutions/bio1/RaindropNumbers/index.tsx
@@ -0,0 +1,15 @@
+import { SCodeBlock, SText, STitle } from "components";
+
+export const RaindropNumbers = () => {
+  return (
+    <>
+      <STitle>Solution</STitle>
+      <SText>
+        Coming Soon!
+      </SText>
+
+      <STitle>Code</STitle>
+      <SCodeBlock path="missing"/>
+    </>
+  );
+};
